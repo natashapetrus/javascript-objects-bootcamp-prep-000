7439372dcf059391766030f8a62d5e1d9307962a
Natasha Petrus <hello@natashapetrus.com>
2017-09-02 07:18:19 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = {'taylor swift' : 'love song'};
 
 function updatePlaylist(playlist, artistName, songTitle){
-  Object.assign(playlist, {[artistName] : songTitle});
+  return Object.assign(playlist, {[artistName] : songTitle});
 }
